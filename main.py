9fc2d5e74c3943ed175a2c4fa4d265e2141702cc
rashevskyv <rashevsky.v@gmail.com>
2024-01-14 18:10:48 +0200

    Now archives and inis generates with NX-Translation API, so no need to redownload and repack translations anymore

--- main.py
+++ main.py
@@ -6,100 +6,24 @@ import time
 from datetime import datetime
 import json
 
-def get_latest_release(user, repo):
-    url = f"https://api.github.com/repos/{user}/{repo}/releases/latest"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.json()
-    except requests.RequestException as e:
-        print(f"Помилка при запиті до GitHub API: {e}")
-        return None
+def download_file(url, target_folder):
+    # Перевірка та створення папки, якщо вона не існує
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+        print(f"Створено папку: {target_folder}")
 
-def download_assets(release_data, target_folder):
-    if not release_data:
-        print("Немає даних для завантаження.")
-        return
+    # Визначення імені файлу з URL
+    file_name = url.split('/')[-1]
+    file_path = os.path.join(target_folder, file_name)
 
-    # Перевірка та створення папки
-    if os.path.exists(target_folder):
-        shutil.rmtree(target_folder)
-    os.makedirs(target_folder)
-
-    assets = release_data.get('assets', [])
-    if not assets:
-        print("Немає архівів для завантаження.")
-        return
-
-    for asset in assets:
-        download_url = asset['browser_download_url']
-        download_asset(download_url, target_folder)
-
-def download_asset(url, target_folder):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        file_name = url.split('/')[-1]
-        file_path = os.path.join(target_folder, file_name)
+    # Завантаження файлу
+    response = requests.get(url)
+    if response.status_code == 200:
         with open(file_path, 'wb') as file:
             file.write(response.content)
-        print(f"Файл '{file_name}' успішно завантажено.")
-    except requests.RequestException as e:
-        print(f"Помилка при завантаженні файлу {url.split('/')[-1]}: {e}")
-
-def unzip_assets(target_folder):
-    for item in os.listdir(target_folder):
-        if item.endswith('.zip'):
-            file_path = os.path.join(target_folder, item)
-            with zipfile.ZipFile(file_path, 'r') as zip_ref:
-                zip_ref.extractall(target_folder)
-            print(f"Файл '{item}' успішно розпаковано.")
-            os.remove(file_path)  # Видалити архів після розпакування
-
-def rename_folders(target_folder):
-    for item in os.listdir(target_folder):
-        if not item.endswith('.ini'):
-            print(f"Item: {item}")
-            # Отримуємо назву папки з назви архіву
-            parts = item.split('_')
-            print(f"Parts: {parts}")
-            folder_name = item
-            print(f"Folder name: {folder_name}")
-            if len(parts) >= 2:
-                new_folder_name = parts[1].capitalize()  # Використовуємо першу частину як нову назву папки
-                original_folder_path = os.path.join(target_folder, folder_name)
-                new_folder_path = os.path.join(target_folder, new_folder_name)
-                if os.path.exists(original_folder_path):
-                    os.rename(original_folder_path, new_folder_path)
-                    print(f"Папка '{folder_name}' перейменована на '{new_folder_name}'.")
-
-def repackage_translations(translation_folder):
-    for language in os.listdir(translation_folder):
-        language_path = os.path.join(translation_folder, language)
-        if os.path.isdir(language_path):
-            for region in os.listdir(language_path):
-                if region == 'licenses':  # Пропускаємо папку licenses
-                    continue
-
-                region_path = os.path.join(language_path, region)
-                if os.path.isdir(region_path):
-                    # Видаляємо префікс "replaces_" для імені архіву
-                    archive_name = region.replace('replaces_', '')
-                    archive_file_path = os.path.join(language_path, archive_name)
-
-                    # Створюємо архів із вмістом папки
-                    shutil.make_archive(archive_file_path, 'zip', region_path)
-                    print(f"Архів '{archive_name}.zip' створено у папці '{language}'.")
-
-                    # Видаляємо оригінальну папку
-                    shutil.rmtree(region_path)
-                    print(f"Папка '{region}' видалена.")
-                    
-def save_last_run_timestamp(file_path):
-    timestamp = int(time.time())
-    with open(file_path, 'w') as file:
-        file.write(str(timestamp))
-    print(f"Мітка часу останнього запуску ({timestamp}) збережена у файлі '{file_path}'.")
+        print(f"Файл {file_name} успішно завантажено в {target_folder}")
+    else:
+        print(f"Помилка при завантаженні файлу: {response.status_code}")
 
 def get_latest_release_date(user, repo):
     url = f"https://api.github.com/repos/{user}/{repo}/releases/latest"
@@ -120,98 +44,6 @@ def get_latest_commit_date(user, repo):
     else:
         print("Немає доступних комітів.")
         return None
-    
-def create_config(translations_path, template_file_path):
-    """
-    Creates a config.ini file based on the updated template file and directories inside the translations folder.
-    This version excludes the 'langs' folder.
-    :param translations_path: Path to the translations folder containing language directories.
-    :param template_file_path: Path to the template file.
-    """
-    try:
-        # Read the template content
-        with open(template_file_path, 'r', encoding='utf-8') as file:
-            template = file.read()
-
-        # List the directories in the given path, excluding 'langs' folder
-        language_dirs = [d for d in os.listdir(translations_path) if os.path.isdir(os.path.join(translations_path, d)) and d != 'langs']
-        
-        # Split the template into two parts: static (first two elements) and dynamic (for each language)
-        static_part, dynamic_part = template.split('[*{%language_name%}]', 1)
-
-        config_content = static_part.strip() + "\n\n"
-
-        # Generate config sections for each language directory
-        for lang in language_dirs:
-            config_section = "[*" + lang + "]" + dynamic_part.replace('{%language_name%}', lang).replace('{%lang%}', lang)
-            config_content += config_section.strip() + "\n\n"
-
-        # Save the config_content to a file in translations_path
-        with open(os.path.join(translations_path, 'config.ini'), 'w', encoding='utf-8') as file:
-            file.write(config_content)
-
-        return "config.ini file created successfully."
-    except Exception as e:
-        return f"Error: {e}"
-        
-def create_json_in_folders(base_path):
-    print("Початок виконання функції...")
-
-    translations_path = os.path.join(base_path, 'translations')
-    print(f"Перевіряємо шлях: {translations_path}")
-
-    if not os.path.exists(translations_path):
-        print("Папка перекладів не знайдена.")
-        return
-    else:
-        print("Папка перекладів знайдена.")
-
-    langs_path = os.path.join(translations_path, 'langs')
-    if not os.path.exists(langs_path):
-        print(f"Створюємо папку: {langs_path}")
-        os.makedirs(langs_path)
-    else:
-        print(f"Папка вже існує: {langs_path}")
-
-    # Словник для розшифровки лінгвістичних пар
-    lang_decoding = {
-        'de-EU': 'German for Europe region',
-        'en-EU': 'English for Europe region',
-        'en-US': 'English for America region',
-        'es-EU': 'Spanish for Europe region',
-        'es-US': 'Spanish for America region',
-        'fr-EU': 'French for Europe region',
-        'it-EU': 'Italian for Europe region',
-        'ru-EU': 'Russian for Europe region'
-    }
-
-    for lang_folder in os.listdir(translations_path):
-        lang_folder_path = os.path.join(translations_path, lang_folder)
-        print(f"Обробляємо папку мови: {lang_folder_path}")
-
-        if os.path.isdir(lang_folder_path) and lang_folder != 'langs':
-            print(f"Створюємо JSON для мови: {lang_folder}")
-            json_array = [{"lang":"`Choose what language will be replaced"},]
-
-            for file in os.listdir(lang_folder_path):
-                if file.endswith('.zip'):
-                    file_url = f"https://github.com/rashevskyv/switch-translations-mirrors/raw/main/translations/{lang_folder}/{file}"
-                    file_name_without_extension = file[:-4]  # Видаляємо розширення .zip
-                    lang_description = lang_decoding.get(file_name_without_extension, file)  # Використовуємо назву файлу, якщо розшифровки немає
-                    json_array.append({
-                        'file-name': file, 
-                        'file-url': file_url,
-                        'lang': lang_description
-                    })
-
-            with open(os.path.join(langs_path, f'{lang_folder}.json'), 'w') as json_file:
-                json.dump(json_array, json_file, indent=4)
-            print(f"JSON файл створено: {lang_folder}.json")
-        else:
-            print(f"Пропускаємо папку: {lang_folder_path}")
-
-    print("Завершення виконання функції.")
-
     
 def archive_and_move(base_path, current_dir):
 	config_file_path = os.path.join(base_path, 'config.ini')
@@ -243,47 +75,114 @@ def archive_and_move(base_path, current_dir):
 
 	print(f"Архів {archive_name} створено і розміщено в {current_dir}")
      
-def sort_starred_sections(file_content):
-    lines = file_content.split('\n')
-    sections = {}
-    starred_sections = {}
-    current_section = None
-    section_count = 0
+def create_config_from_json(api_data, template_path, config):
+    print("Reading template file...")
+    with open(template_path, 'r') as file:
+        template = file.read()
 
-    for line in lines:
-        if line.startswith('[') and line.endswith(']'):
-            current_section = line[1:-1]
-            section_count += 1
-            if '*' in current_section and section_count > 2:
-                starred_sections[current_section] = []
-            else:
-                sections[current_section] = []
-        elif current_section is not None:
-            if current_section in starred_sections:
-                starred_sections[current_section].append(line)
-            else:
-                sections[current_section].append(line)
+    print("Extracting languages...")
+    languages = api_data.get('languages', [])
+    
+    print("Sorting languages...")
+    languages_sorted = sorted(languages, key=lambda x: x['id'])
 
-    sorted_starred_sections = {k: v for k, v in sorted(starred_sections.items())}
+    print("Initializing config content...")
+    config_content = template.split("[*{%name%}]")[0]
 
-    sorted_content = []
-    for section, lines in sections.items():
-        sorted_content.append(f'[{section}]')
-        sorted_content.extend(lines)
-        sorted_content.append('')
-    for section, lines in sorted_starred_sections.items():
-        sorted_content.append(f'[{section}]')
-        sorted_content.extend(lines)
-        sorted_content.append('')
+    for lang in languages_sorted:
+        print(f"Processing language: {lang['name']}")
+        language_section = template.split("[*{%name%}]")[1]
+        language_section = language_section.replace("{%name%}", lang['name'])
+        language_section = language_section.replace("{%url%}", lang['download_url'])
+        language_section = language_section.replace("{%id%}", lang['id'])
 
-    return '\n'.join(sorted_content)
+        config_content += "\n\n[*{0}]\n".format(lang['name']) + language_section
+
+    config_ini_path = config
+
+    # Перевірка та створення директорії, якщо потрібно
+    config_dir = os.path.dirname(config_ini_path)
+    if not os.path.exists(config_dir):
+        print(f"Creating directory: {config_dir}")
+        os.makedirs(config_dir)
+
+    print("Saving config file...")
+    with open(config_ini_path, 'w') as file:
+        file.write(config_content)
+    print("Config file created successfully at:", config_ini_path)
+
+def create_language_jsons(api_data, base_path):
+    translations_path = os.path.join(base_path, 'translations')
+
+    langs_path = os.path.join(translations_path, 'langs')
+    
+    # Створення папки для мов, якщо вона не існує
+    if not os.path.exists(langs_path):
+        print(f"Створюємо папку для мов: {langs_path}")
+        os.makedirs(langs_path)
+    else:
+        print(f"Папка для мов вже існує: {langs_path}")
+
+    lang_decoding = {
+        'replaces_CN-zh': 'Chinese for China region',
+        'replaces_EU-de': 'German for Europe region',
+        'replaces_EU-en': 'English for Europe region',
+        'replaces_EU-es': 'Spanish for Europe region',
+        'replaces_EU-fr': 'French for Europe region',
+        'replaces_EU-it': 'Italian for Europe region',
+        'replaces_EU-nl': 'Dutch for Europe region',
+        'replaces_EU-pt': 'Portuguese for Europe region',
+        'replaces_EU-ru': 'Russian for Europe region',
+        'replaces_JP-ja': 'Japanese for Japan region',
+        'replaces_KR-ko': 'Korean for Korea region',
+        'replaces_TW-zh': 'Chinese for Taiwan region',
+        'replaces_US-en': 'English for America region',
+        'replaces_US-es': 'Spanish for America region',
+        'replaces_US-fr': 'French for America region',
+        'replaces_US-pt': 'Portuguese for America region'
+    }
+
+    print("Обробка даних мов...")
+    for lang in api_data['languages']:
+        json_structure = [{"lang": "`Choose what language will be replaced"}]
+
+        for replace in lang['replaces']:
+            decoding_key = f"replaces_{replace['region']['id']}-{replace['locale']['id']}"
+            decoded_lang = lang_decoding.get(decoding_key, "")
+            json_structure.append({
+                "lang": decoded_lang,
+                "dir": replace['path']
+            })
+
+        json_filename = f"{lang['name']}.json"
+        json_file_path = os.path.join(langs_path, json_filename)
+
+        # Запис у JSON файл
+        with open(json_file_path, 'w') as json_file:
+            json.dump(json_structure, json_file, indent=4)
+            print(f"JSON файл створено: {json_filename}")
+
+    print("Створення JSON файлів завершено.")
+
+def read_json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            print(f"Відкриваю файл: {file_path}")
+            data = json.load(file)
+            print("Файл успішно прочитано.")
+            return data
+    except FileNotFoundError:
+        print(f"Помилка: файл не знайдено - {file_path}")
+    except json.JSONDecodeError:
+        print(f"Помилка: не вдалося декодувати JSON з файлу - {file_path}")
+    except Exception as e:
+        print(f"Виникла помилка при читанні файлу: {e}")
 
 def main():
     user = 'NX-Family'
     repo = 'NX-Translation'
     current_dir = os.path.dirname(os.path.realpath(__file__))
     target_folder = os.path.join(current_dir, 'translations')
-    release_data = get_latest_release(user, repo)
     translations_path = target_folder
     commit_user = 'rashevskyv'  # замініть на потрібного користувача
     commit_repo = 'switch-translations-mirrors'  # замініть на потрібний репозиторій
@@ -291,33 +190,22 @@ def main():
     release_date = get_latest_release_date(user, repo)
     commit_date = get_latest_commit_date(commit_user, commit_repo)
 
-        
-    # download_assets(release_data, target_folder)
-    # unzip_assets(target_folder)
-    # rename_folders(target_folder)
-
-    # repackage_translations(translations_path)
-
-    # create_config(translations_path, os.path.join(current_dir, 'config_template.ini'))
-    # create_json_in_folders(current_dir)
-
-    # sort_starred_sections(os.path.join(translations_path, 'config.ini'))
+    # download_file('https://raw.githubusercontent.com/NX-Family/NX-Translation/main/api.json', current_dir)
+    # api_data = read_json(os.path.join(current_dir, 'api.json'))
+    # create_config_from_json(api_data, os.path.join(current_dir, 'config_template.ini'), os.path.join(translations_path, 'config.ini'))
+    # create_language_jsons(api_data, current_dir)
     # archive_and_move(translations_path, current_dir)
 
     if release_date and commit_date:
         if commit_date < release_date:
             print(f"New release was detected on {release_date.strftime('%d.%m.%Y %H:%M:%S')}.")	
-            download_assets(release_data, target_folder)
-            unzip_assets(target_folder)
-            rename_folders(target_folder)
 
-            repackage_translations(translations_path)
-
-            create_config(translations_path, os.path.join(current_dir, 'config_template.ini'))
-            create_json_in_folders(current_dir)
-
-            sort_starred_sections(os.path.join(translations_path, 'config.ini'))
+            download_file('https://raw.githubusercontent.com/NX-Family/NX-Translation/main/api.json', current_dir)
+            api_data = read_json(os.path.join(current_dir, 'api.json'))
+            create_config_from_json(api_data, os.path.join(current_dir, 'config_template.ini'), os.path.join(translations_path, 'config.ini'))
+            create_language_jsons(api_data, current_dir)
             archive_and_move(translations_path, current_dir)
+
         else:
             print(f"No new releases detected")
     else:
